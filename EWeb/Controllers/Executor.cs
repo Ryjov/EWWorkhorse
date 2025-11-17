2a7213531d4859b5183ca426f900e0f71eac5993
Egor Ryzhov <32880315+Ryjov@users.noreply.github.com>
2025-11-17 16:23:42 +0300

    EWeb controllers
    Took business logic out of controller, removed some unnecessay stuff

---
+++ EWeb/Controllers/Executor.cs
@@ -0,0 +1,40 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using EWeb.RPC;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EWeb.Controllers
+{
+    public static class Executor
+    {
+        public static async Task ExecuteAsync(IFormFileCollection uploadedFiles, MemoryStream stream)
+        {
+            var file = await InvokeRPCAsync(uploadedFiles);
+            var beginningTagLength = 1348;
+            var endingTagLength = 22;
+            file = file.Substring(1348, file.Length - beginningTagLength - endingTagLength);
+
+            using (WordprocessingDocument doc = WordprocessingDocument.Create(stream, DocumentFormat.OpenXml.WordprocessingDocumentType.Document, true))
+            {
+                MainDocumentPart mainPart = doc.AddMainDocumentPart();
+
+                new Document(new Body()).Save(mainPart);
+
+                Body body = mainPart.Document.Body;
+                body.InnerXml = file;
+
+                mainPart.Document.Save();
+            }
+            stream.Seek(0, SeekOrigin.Begin);
+        }
+
+        private static async Task<string> InvokeRPCAsync(IFormFileCollection files)
+        {
+            var rpcClient = new RPCClient();
+            await rpcClient.StartAsync();
+            var resultFile = await rpcClient.CallAsync(files);
+
+            return resultFile;
+        }
+    }
+}
--- EWeb/Controllers/HomeController.cs
+++ EWeb/Controllers/HomeController.cs
@@ -36,57 +36,10 @@ namespace EWeb.Controllers
         [HttpPost]
         public async Task<IActionResult> AddFile(IFormFileCollection uploadedFiles)
         {
-            var file = await InvokeAsync(uploadedFiles);
-            var beginningTagLength = 1348;
-            var endingTagLength = 22;
-            file = file.Substring(1348, file.Length - beginningTagLength - endingTagLength);
-
             var stream = new MemoryStream();
-            using (WordprocessingDocument doc = WordprocessingDocument.Create(stream, DocumentFormat.OpenXml.WordprocessingDocumentType.Document, true))
-            {
-                MainDocumentPart mainPart = doc.AddMainDocumentPart();
-
-                new Document(new Body()).Save(mainPart);
-
-                Body body = mainPart.Document.Body;
-                body.InnerXml = file;
-
-                mainPart.Document.Save();
-            }
-            stream.Seek(0, SeekOrigin.Begin);
+            await Executor.ExecuteAsync(uploadedFiles, stream);
 
             return File(stream, "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "result.docx");
-        }
-
-        [HttpGet]
-        public async Task GetResult()//FileStreamResult
-        {
-            ConnectionFactory factory = new();
-            factory.Uri = new Uri(uriString: "amqp://guest:guest@localhost:1011");
-            factory.ClientProvidedName = "Rabbit receiver app";
-
-
-            var cnn = await factory.CreateConnectionAsync();
-            var channel = await cnn.CreateChannelAsync();
-
-            string exchangeName = "EWExchange";
-            string routingKey = "ew-routing-key";
-            string queueName = "EWFileQueue";
-
-            await channel.ExchangeDeclareAsync(exchangeName, ExchangeType.Direct);
-            await channel.QueueDeclareAsync(queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
-            await channel.QueueBindAsync(queueName, exchangeName, routingKey, arguments: null);
-            await channel.BasicQosAsync(prefetchSize: 0, prefetchCount: 2, global: false);
-
-            var consumer = new AsyncEventingBasicConsumer(channel);
-            while (!_cancellationToken.IsCancellationRequested)
-            {
-                consumer.ReceivedAsync += async (sender, args) =>
-                {
-                    await channel.BasicAckAsync(args.DeliveryTag, multiple: true);
-                    var wordBody = args.Body.ToArray();
-                };
-            }
         }
 
         public IActionResult Privacy()
@@ -98,15 +51,6 @@ namespace EWeb.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
-        private static async Task<string> InvokeAsync(IFormFileCollection files)
-        {
-            var rpcClient = new RPCClient();
-            await rpcClient.StartAsync();
-            var resultFile = await rpcClient.CallAsync(files);
-
-            return resultFile;
         }
     }
 }
